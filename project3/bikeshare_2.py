3d7a28d1906a67b501ebe56771708e852f0077ee
Jenniferxdxd <jenniferlky911@gmail.com>
2023-08-27 15:22:47 +0800

    refactoring bikeshare.py

--- project3/bikeshare_2.py
+++ project3/bikeshare_2.py
@@ -2,6 +2,7 @@ import time
 import pandas as pd
 import numpy as np
 
+# data source files start
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
